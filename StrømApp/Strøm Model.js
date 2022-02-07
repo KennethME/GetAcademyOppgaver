0a6e493a313ad361d79cbcd029a2eb106c2afb8c
KennethME <kennethe@getacademy.no>
2022-02-07 12:38:16 +0100

    update

--- StrømApp/Strøm Model.js
+++ StrømApp/Strøm Model.js
@@ -1,30 +1,30 @@
+var today = new Date();
+var time = addZero(today.getHours())+':'+addZero(today.getMinutes())+':'+addZero(today.getSeconds());
+var dato = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+function addZero(i){
+    if (i < 10) {i = "0" + i}
+    return i;
+}
+
+var idag = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+'T'+addZero(today.getHours())+":00:00+01:00";
+
 const api_url = 
-    "https://norway-power.ffail.win/?zone=NO1&date=2020-02-17";
+    "https://norway-power.ffail.win/?zone=NO1&date="+dato;
 
 async function getApi(url) {
     const response = await fetch(url);
-
     var data = await response.json();
-    console.log(data);
-    
-    if (response) {
-        hideLoader();
-    }
-    model = data;
-    updateView(data)
+    model.dagLog = data;
+    model.timeLog = model.dagLog[idag];
+    updateView()
 }
 getApi(api_url);
-function hideLoader() {
-    
-}
+
+var kwhiTimen = '';
+
 
 var model =
     {
-
+        dagLog: '',
+        timeLog: '',
     };
-// const model = 
-//     {
-//     name: '',
-//     age: '',
-//     count: '',
-//     };
--- StrømApp/Strøm.js
+++ StrømApp/Strøm.js
@@ -2,9 +2,28 @@ updateView()
     function updateView()
     {
         document.getElementById('app').innerHTML = `
-        <tr>
-          <th>Name:</th><br>
-          <th>Age:</th><br>
-          <th>Count:</th><br>
-         </tr>`;
+        <div class="textBox">
+            <tr>
+                <th>Dato: ${dato}</th><br>
+                <th>Tid: ${time}</th><br>
+                <th>Nok per kWh: ${model.timeLog.NOK_per_kWh}</th><br>
+                <th>kWh i Time: ${kwhiTimen}kWh</th><br>
+                <th>Pris: ${model.timeLog.NOK_per_kWh * kwhiTimen}kr</th><br>
+            </tr>
+        </div>
+
+        <div class="termoBox">
+            <div>
+            Varmegrader: 20c
+            </div>
+            <div class="column">
+            <button type="">Opp</button>
+            <button type="">Ned</button>
+            </div>
+
+        
+        
+        
+        `;
+        
     }
